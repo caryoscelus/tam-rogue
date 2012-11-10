9d8fd6a29fd9acee0581566c285b7ebc75e44fa3
caryoscelus <caryoscelus@gmx.com>
2012-11-10 20:52:53 +0200

    map editor update

--- src/main/mapeditor.py
+++ src/main/mapeditor.py
@@ -1,16 +1,37 @@
 #! /usr/bin/env python3
 
 import logging
+import threading
 
-class MapEditor:
+from sleeping import Sleeping
+
+class MapEditor(Sleeping):
     def __init__(self):
-        pass
+        self.quit = False
     
     def start(self):
-        pass
+        self.updaterThread = threading.Thread(None, self.clientUpdater, 'updater')
+        self.updaterThread.start()
+    
+    # thread
+    def clientUpdater(self):
+        while not self.quit:
+            try:
+                if self.updateWorld:
+                    self.redraw()
+                self.sleep()
+            except Exception as err:
+                logging.error('unhandled exception in clientUpdater thread:')
+                logging.error(str(err))
+                logging.debug(traceback.format_exc())
 
 
 if __name__ == '__main__':
+    from sys import argv
+    
+    if len(argv) < 2:
+        raise RuntimeError('not enough command line arguments')
+    
     logging.basicConfig(filename='mapeditor.log', level=logging.DEBUG)
     
     me = MapEditor()
