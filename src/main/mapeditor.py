8788d414c00c49e952e238754d315f737f1a8698
caryoscelus <caryoscelus@gmx.com>
2012-11-10 19:09:28 +0200

    mapeditor placeholder

---
+++ src/main/mapeditor.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import logging
+
+class MapEditor:
+    def __init__(self):
+        pass
+    
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='mapeditor.log', level=logging.DEBUG)
+    
+    me = MapEditor()
+    me.start()
