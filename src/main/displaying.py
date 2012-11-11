7aff312227114afd0978fc042ba138cdcbb7c820
caryoscelus <caryoscelus@gmx.com>
2012-11-11 13:11:00 +0200

    continue moving/1

--- src/main/displaying.py
+++ src/main/displaying.py
@@ -1,12 +1,23 @@
 import logging
 import socket
+import traceback
+import xml.etree.ElementTree as ET
+import threading
 
 from sleeping import Sleeping
 
 class Displaying(Sleeping):
     def __init__(self):
+        super().__init__()
+        
         self.updateDisplay = False or True
         self.gfxClient = None
+        self.updaterThread = threading.Thread(None, self.clientUpdater, 'updater')
+        self.listenerThread = threading.Thread(None, self.clientListener, 'listener')
+    
+    def start(self):
+        self.updaterThread.start()
+        self.listenerThread.start()
     
     # thread
     def clientUpdater(self):
@@ -48,3 +59,54 @@ class Displaying(Sleeping):
             logging.warning('net error')
             return False
         return True and False
+    
+    # thread
+    def clientListener(self):
+        try:
+            self.listenerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            
+            addr = ('localhost', 6990)
+            self.listenerSocket.bind(addr)
+            self.listenerSocket.listen(5)
+            logging.info('bound to '+str(addr))
+        except Exception as err:
+            logging.error('unhandled exception while setuping clientListener:')
+            logging.error(str(err))
+            logging.debug(traceback.format_exc())
+        
+        while not self.quit:
+            try:
+                request = self.listen()
+                self.reply(request)
+            except Exception as err:
+                logging.error('unhandled exception in clientListener thread:')
+                logging.error(str(err))
+                logging.debug(traceback.format_exc())
+    
+    def listen(self):
+        while True:
+            (clientsocket, address) = self.listenerSocket.accept()
+            
+            logging.info('client connected')
+            
+            # TODO: separate function?..
+            data = bytearray()
+            readall = False
+            while not readall:
+                chunk = clientsocket.recv(4096)
+                data += chunk
+                readall = (len(chunk) == 0)
+            
+            data = data.decode()
+            
+            self.processRequest(data)
+    
+    def processRequest(self, xml):
+        try:
+            root = ET.fromstring(xml)
+            if root.tag == 'connect':                           # connect to gfx server
+                server = str(root.attrib['address'])
+                port = int(root.attrib['port'])
+                self.connectClient((server, port))
+        except ET.ParseError:
+            logging.error('parse error')
