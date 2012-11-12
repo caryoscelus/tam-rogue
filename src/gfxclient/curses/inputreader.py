f5909a74e7a1995bc83a7e559f37f1eef4d1be0b
caryoscelus <caryoscelus@gmx.com>
2012-11-12 23:40:06 +0200

    obvious fix

--- src/gfxclient/curses/inputreader.py
+++ src/gfxclient/curses/inputreader.py
@@ -4,15 +4,17 @@ import xml.etree.ElementTree as ET
 class InputReader:
     def __init__(self, disp, backSender):
         self.disp = disp
+        self.backSender = backSender
     
     def loop(self):
         while not self.disp.quit:
             try:
                 opcode = self.disp.cScr.getch()
                 self.processKey(opcode)
-            except:
+            except Exception as err:
                 logging.error('unhandled exception in input reader')
+                logging.debug(err)
     
     def processKey(self, opcode):
-        root = ET.Element('input', {'opcode':opcode})
-        self.backSender.send(root.toString())
+        root = ET.Element('input', {'opcode':str(opcode)})
+        self.backSender.send(ET.tostring(root))
