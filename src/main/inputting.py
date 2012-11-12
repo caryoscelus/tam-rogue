670a6fe1df4cc91a0121d3d20654b6bb83ef1bae
caryoscelus <caryoscelus@gmx.com>
2012-11-13 00:01:52 +0200

    inputting, receiver

--- src/main/inputting.py
+++ src/main/inputting.py
@@ -1,8 +1,38 @@
 import logging
+import xml.etree.ElementTree as ET
+
+from receiver import Receiver
 
 class Inputting:
     def __init__(self):
-        pass
+        self.receiver = Receiver()
+        self.quit = False
+    
+    def connect(self, cl):
+        self.receiver.connect(cl)
+    
+    def loop(self):
+        while not self.quit:
+            try:
+                key = self.readKey()
+                processKey(key)
+            except Exception as err:
+                logging.error('error while waiting key')
+                logging.debug(err)
+    
+    def readKey(self):
+        key = None
+        while not key:
+            xml = self.listen()
+            root = ET.fromstring(xml)
+            if root.tag == 'input':
+                try:
+                    key = root.attrib['opcode']
+                except Exception as err:
+                    logging.warning('bad xml')
+            else:
+                logging.warning('unknown xml node type')
+        return key
     
     def processKey(self, opcode):
         try:
---
+++ src/main/receiver.py
@@ -0,0 +1,13 @@
+import logging
+import socket
+
+class Receiver:
+    def __init__(self, addr = None):
+        self.connect(addr)
+    
+    def connect(self, addr):
+        self.addr = addr
+    
+    def listen():
+        logging.debug('listening..')
+        return None
