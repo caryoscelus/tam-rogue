755ec62ef7260c52e49d268a755a2adf4b2fceb0
caryoscelus <caryoscelus@gmx.com>
2012-11-16 01:46:04 +0200

    small fix

--- src/main/actionapi.py
+++ src/main/actionapi.py
@@ -2,8 +2,14 @@
 from builtins import Exception, \
                      int, str, dict, list
 
-from logging import info, warning, error
+from logging import debug, info, warning, error
+
+from entity import EntityAttributeError
 
 def action(name, args):
     import worldregistry
     worldregistry.sysWorldRegistry.world.actions[name].applyAction(args)
+
+def traceback():
+    import traceback
+    debug(traceback.format_exc())
