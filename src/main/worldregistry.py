204eca08bd0068371f0ece26010118064afcdd4f
caryoscelus <caryoscelus@gmx.com>
2012-11-20 19:19:40 +0200

    error handling

--- src/main/worldregistry.py
+++ src/main/worldregistry.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import logging
 
 from mod import Mod
 from world import World
@@ -10,16 +11,23 @@ class WorldRegistry:
         self.world = World()
     
     def loadMod(self, modFile):
-        f = open(modFile)
-        modXml = f.read()
-        f.close()
-        
-        newMod = Mod(ET.fromstring(modXml))
-        self.lastMod += 1
-        self.mods[self.lastMod] = newMod
-        newMod.applyMod(self.world)
-        
-        return self.lastMod
+        try:
+            f = open(modFile)
+            modXml = f.read()
+            f.close()
+            
+            newMod = Mod(ET.fromstring(modXml))
+            
+            self.lastMod += 1
+            self.mods[self.lastMod] = newMod
+            newMod.applyMod(self.world)
+            
+            return self.lastMod
+        except IOError:
+            logging.warning('cannot load mod file {0}'.format(modFile))
+        except ET.ParseError:
+            logging.warning('cannot parse mod file {0}'.format(modFile))
+        return None
     
     def mod(self, modId):
         return self.mods[modId]
