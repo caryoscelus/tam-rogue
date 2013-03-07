d1d7d2b53da9b263cda3c9d949f62a0ffe864f70
caryoscelus <caryoscelus@gmx.com>
2013-03-07 22:57:49 +0400

    simple bot

--- src/main/botclient.py
+++ src/main/botclient.py
@@ -4,4 +4,42 @@ from baseclient import BaseClient
 
 class BotClient(BaseClient):
     def requestAction(self):
-        logging.warning('bot should move, but don\'t know how')
+        if self.attack():
+            return
+        
+        if self.move():
+            return
+        
+        self.wait()
+    
+    def move(self):
+        for x in range(-1, 1):
+            for y in range(-1, 1):
+                if x or y:
+                    try:
+                        self.moveTo(x, y)
+                        return True
+                    except RuntimeError:
+                        pass
+        return False
+    
+    def wait(self):
+        # TODO: wait
+        pass
+    
+    def attack(self):
+        for x in range(-1, 1):
+            for y in range(-1, 1):
+                if x or y:
+                    t = self.getTile(x, y)
+                    monster = t.get('monster')
+                    if monster:
+                        self.attackEnemy(monster)
+                        return True
+        return False
+    
+    def moveTo(self, dx, dy):
+        self.doAction('move', {'subject':self, 'dx':dx, 'dy':dy})
+    
+    def attackEnemy(self, enemy):
+        self.doAction('fight', {'subject':self, 'target':enemy})
