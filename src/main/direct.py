b0a8327cae63aed8c9bcd32ab8db8a83bd2cdf65
caryoscelus <caryoscelus@gmx.com>
2013-03-11 00:44:17 +0400

    direction error handling

--- src/main/direct.py
+++ src/main/direct.py
@@ -1,7 +1,11 @@
 def Direct(x, y):
     '''Pack x, y direction to one integer'''
+    if not x in (-1, 2) or not y in (-1, 2):
+        raise ValueError('direction x, y not in range')
     return (x+1)*3+(y+1)
 
 def UnDirect(d):
     '''Convert integer packed direction to x, y'''
+    if not d in range(9):
+        raise ValueError('direction not in range')
     return d//3-1, d%3-1
